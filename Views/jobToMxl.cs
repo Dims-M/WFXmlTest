40194d96c75c2254ab9d7f9600badde9a7a73937
Dim <Dim@DESKTOP-2JEO7BL>
2020-02-26 17:56:34 +0300

    Добавленная строка

--- Views/jobToMxl.cs
+++ Views/jobToMxl.cs
@@ -16,5 +16,27 @@ namespace WFXmlTest.Views
         {
             InitializeComponent();
         }
+
+        //кнопка добавить. Из текстбоксов перенос в таблицу
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Заполните все поля!!!!");
+            }
+
+            else
+            {
+                int n = dataGridView1.Rows.Add(); // создаем новую строку
+                dataGridView1.Rows[n].Cells[0].Value = textBox1.Text; // столбец Name
+                dataGridView1.Rows[n].Cells[1].Value = numericUpDown1.Value; // Age
+                dataGridView1.Rows[n].Cells[2].Value = textBox2.Text; // Programmer
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
